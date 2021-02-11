2a78492c349582f6855b8ebe6d3fe6e12d39e9b8
Kajal-More <56627613+MoreKajal@users.noreply.github.com>
2021-02-11 08:49:36 -0800

    Update EmpWageBuilder.java

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -11,24 +11,24 @@ public class EmpWageBuilder
 			switch (empCheck ) {
 				case IS_PRESENT:
 					empHrs = 8;
-               break;
-            case IS_PART_TIME:
-               empHrs = 4;
-               break;
-            default:
-               empHrs = 0;
-         }
-         totalEmpHrs += empHrs;
-         System.out.println("For Day # : " + totalWorkingDays + " Worked EmpHrs are : " + empHrs );
-      }
-      int totalEmpWage = totalEmpHrs * empRate;
-      System.out.println("Total Employee Wage for Company " + companyName + " is :"+ totalEmpWage);
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+               				break;
+            			default:
+               				empHrs = 0;
+         		}
+         		totalEmpHrs += empHrs;
+         		System.out.println("For Day # : " + totalWorkingDays + " Worked EmpHrs are : " + empHrs );
+      		}
+      		int totalEmpWage = totalEmpHrs * empRate;
+      		System.out.println("Total Employee Wage for Company " + companyName + " is :"+ totalEmpWage);
 	}
 	
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-      calEmpWageForCompany("DMART", 20, 2, 100);
-      calEmpWageForCompany("VISION", 400, 8, 16);
+      		calEmpWageForCompany("DMART", 20, 2, 100);
+      		calEmpWageForCompany("VISION", 400, 8, 16);
 	}
 }
 
