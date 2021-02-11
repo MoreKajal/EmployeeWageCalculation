ba8df0baabab7e4044eced3235c3b9a5bfa3c1dd
MoreKajal <kajalm3105@gmail.com>
2021-02-10 22:28:42 -0800

    [Kajal]Added:Monthly Wage Calculation Code

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,22 +3,25 @@ public class EmpWageBuilder
 	public static final int IS_PRESENT = 1;
 	public static final int IS_PART_TIME = 2;
         public static final int WAGE_RATE = 20;
+	public static final int WORKING_DAYS = 20;
         public static void main(String args[]) {
                 System.out.println("Welcome to Employee Wage Computation Program");
-		int empHrs = 0, empWage = 0;
-	        int empCheck = (int)Math.floor(Math.random() * 10 ) % 3;
-        	switch (empCheck ) {
-			case IS_PRESENT:
-				empHrs = 8;
-				break;
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-              		default:
-                		empHrs = 0;
-		}
+		int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		for (int day = 0; day < WORKING_DAYS; day++ ) {
+		        int empCheck = (int)Math.floor(Math.random() * 10 ) % 3;
+        		switch (empCheck ) {
+				case IS_PRESENT:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+              			default:
+                			empHrs = 0;
+			}
 		empWage = empHrs * WAGE_RATE;
-		System.out.println("Employee Wage is : " + empWage);
-
+		totalEmpWage += empWage;
+		}
+		System.out.println("Total Employee Wage is : " + totalEmpWage);
         }
 }
