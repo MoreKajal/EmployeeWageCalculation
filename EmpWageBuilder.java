85f17fcdb4399bdfaf7e19cbbbe42e7128dc6be5
MoreKajal <kajalm3105@gmail.com>
2021-02-10 22:04:59 -0800

    [Kajal]Added:Daily Emp Wage Code

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,13 +1,19 @@
 public class EmpWageBuilder
 {
+	public static final int IS_PRESENT = 1;
+        public static final int WAGE_RATE = 20;
+
         public static void main(String args[]) {
                 System.out.println("Welcome to Employee Wage Computation Program");
-		int IS_FULL_TIME = 1;
+		int empHrs = 0;
+		int empWage = 0;
 	        double empCheck = Math.floor(Math.random() * 10 ) % 2;
-        	if (empCheck == IS_FULL_TIME)
-                	System.out.println("Employee is Present");
-        	else
-                	System.out.println("Employee is Abscent");
+        	if (empCheck == IS_PRESENT)
+			empHrs = 8;
+              	else
+                	empHrs = 0;
+		empWage = empHrs * WAGE_RATE;
+		System.out.println("Employee Wage is : " + empWage);
 
         }
 }
