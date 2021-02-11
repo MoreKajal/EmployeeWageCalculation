cd84f0c2bbb9e7fba8528ab1d68a71dc7bb5a3d4
MoreKajal <kajalm3105@gmail.com>
2021-02-10 22:12:25 -0800

    [Kajal]Added:Part Time Emp Code

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,15 +1,17 @@
 public class EmpWageBuilder
 {
 	public static final int IS_PRESENT = 1;
+	public static final int IS_PART_TIME = 2;
         public static final int WAGE_RATE = 20;
-
         public static void main(String args[]) {
                 System.out.println("Welcome to Employee Wage Computation Program");
 		int empHrs = 0;
 		int empWage = 0;
-	        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+	        double empCheck = Math.floor(Math.random() * 10 ) % 3;
         	if (empCheck == IS_PRESENT)
 			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
               	else
                 	empHrs = 0;
 		empWage = empHrs * WAGE_RATE;
