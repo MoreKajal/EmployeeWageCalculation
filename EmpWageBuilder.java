7fd251857b7ba68d725ef09edf12288c43e6cfa9
MoreKajal <kajalm3105@gmail.com>
2021-02-10 22:20:02 -0800

    [Kajal]Added:Code for Switch Case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,15 +5,18 @@ public class EmpWageBuilder
         public static final int WAGE_RATE = 20;
         public static void main(String args[]) {
                 System.out.println("Welcome to Employee Wage Computation Program");
-		int empHrs = 0;
-		int empWage = 0;
-	        double empCheck = Math.floor(Math.random() * 10 ) % 3;
-        	if (empCheck == IS_PRESENT)
-			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-              	else
-                	empHrs = 0;
+		int empHrs = 0, empWage = 0;
+	        int empCheck = (int)Math.floor(Math.random() * 10 ) % 3;
+        	switch (empCheck ) {
+			case IS_PRESENT:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+              		default:
+                		empHrs = 0;
+		}
 		empWage = empHrs * WAGE_RATE;
 		System.out.println("Employee Wage is : " + empWage);
 
