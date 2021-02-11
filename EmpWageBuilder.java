3d5ab3f55115df848d199bb5183f54692ec7ce25
MoreKajal <kajalm3105@gmail.com>
2021-02-10 22:39:42 -0800

    [Kajal]Added:Code for Total Working Hrs reached

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,10 +4,12 @@ public class EmpWageBuilder
 	public static final int IS_PART_TIME = 2;
         public static final int WAGE_RATE = 20;
 	public static final int WORKING_DAYS = 20;
+	public static final int MAX_HRS = 100;
         public static void main(String args[]) {
                 System.out.println("Welcome to Employee Wage Computation Program");
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
-		for (int day = 0; day < WORKING_DAYS; day++ ) {
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS && totalWorkingDays < WORKING_DAYS ) {
+			totalWorkingDays++;
 		        int empCheck = (int)Math.floor(Math.random() * 10 ) % 3;
         		switch (empCheck ) {
 				case IS_PRESENT:
@@ -19,9 +21,10 @@ public class EmpWageBuilder
               			default:
                 			empHrs = 0;
 			}
-		empWage = empHrs * WAGE_RATE;
-		totalEmpWage += empWage;
+			totalEmpHrs += empHrs;
+			System.out.println("On Day : " + totalWorkingDays + " Worked EmpHrs are : " + empHrs );
 		}
+		int totalEmpWage = totalEmpHrs * WAGE_RATE;
 		System.out.println("Total Employee Wage is : " + totalEmpWage);
         }
 }
